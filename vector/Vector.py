03c57c674c3b5692cb04f54c5ff5e0c1e0ee968e
Frank Schubert <f.schubert@gmail.com>
2022-04-13 15:34:45 -0700

    comments in Vector class

--- vector/Vector.py
+++ vector/Vector.py
@@ -12,19 +12,22 @@ class Vector:
 	def __abs__(self):
 		return hypot(self.x, self.y)
 
+	# Addition of Vector types
 	def __add__(self, other):
 		x = self.x + other.x
 		y = self.y + other.y
 		return Vector(x, y)
 	
+	# Subtraction of Vector types
 	def __sub__(self, other):
 		x = self.x - other.x
 		y = self.y - other.y
 		return Vector(x, y)
-	
+
+	# Multiplication: Vector on the left of the operation
 	def __mul__(self, scalar):
 		return Vector(self.x * scalar, self.y * scalar)
 
+	# Multiplication: allow Vector to be on the right of the operation
 	def __rmul__(self, scalar):
 		return Vector(self.x * scalar, self.y * scalar)
-
