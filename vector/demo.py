750de0b0022da16073367417ae72e6536b21bb84
Frank Schubert <f.schubert@gmail.com>
2022-04-15 10:52:05 -0700

    update print statements for python 3

--- vector/demo.py
+++ vector/demo.py
@@ -3,23 +3,23 @@ from Vector import Vector
 v1 = Vector(2, 4)
 v2 = Vector(2, 1)
 
-print v1
-print v2
+print(v1)
+print(v2)
 
 
 v3 = v1 + v2
-print 'v1 + v2 = ', v3
+print('v1 + v2 = ', v3)
 
 v4 = v1 - v2
-print 'v1 - v2 = ', v4
+print('v1 - v2 = ', v4)
 
 v4 = v1 * 5
-print 'v1 * 5 = ', v4
+print('v1 * 5 = ', v4)
 
 v5 = 5 * v1
-print '5 * v1 = ', v5
+print('5 * v1 = ', v5)
 
 v3 = Vector(-6, 8)
 v4 = Vector(5, 12)
 scalar = v3.dot(v4)
-print scalar
+print(scalar)
